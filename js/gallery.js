954aa8e3b8538a9f61b0acf1942f5249e7af10cd
Muzafar Ahmed <ahmed.muzafar@gmail.com>
2020-02-23 13:14:28 +0100

    Add pre-loading for large images and clean up JS file

--- js/gallery.js
+++ js/gallery.js
@@ -1,38 +1,31 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo img");
 
-  // Select all images under the gallery-thumbs id
-  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img")
-  // bind to the main image
-  let mainImage = document.querySelector("#gallery-photo > img");
-
-// Set up an event listener for each selection from 'thumbnails'
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
+      // Set clicked image as display image.
+      mainImage.setAttribute("src", newImageSrc);
 
-     // get the newImageSrc (from dataset)
-     // and get the newImageAlt (from image attributes)
-     let newImageSrc = thumbnail.dataset.largeVersion;
-     // let newImageAlt = thumbnail.dataset.title;
-     let newImageAlt = thumbnail.getAttribute("alt");
+      // Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
 
-     mainImage.setAttribute("src", newImageSrc);
-     mainImage.setAttribute("alt", newImageAlt)
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
 
-     // Have the current class applied to current selection
-     // and have it removed from the non-current selection
-     let currentClass = "current"
-     document.querySelector(".current").classList.remove(currentClass);
-     thumbnail.parentNode.classList.add(currentClass);
-
-     // Get the DOM elements for the image title and description
-     // Then modify thier innerHTML to the selected thumbnails
-     let galleryInfo = document.querySelector("#gallery-info")
-     let title       = galleryInfo.querySelector(".title")
-     let description  = galleryInfo.querySelector(".description")
-
-     title.innerHTML      = thumbnail.dataset.title;
-     description.innerHTML = thumbnail.dataset.description;
-
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
